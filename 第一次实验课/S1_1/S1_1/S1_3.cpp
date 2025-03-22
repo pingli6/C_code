28dc2e0ed875a8a4c526d941d84cd2e99e8ef1b7
pingli6 <lip3ing@gmail.com>
2025-03-22 21:50:51 +0800

    第一次的实验

---
+++ 第一次实验课/S1_1/S1_1/S1_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	const double Pi = 3.14159;
+	double r,s;
+	cout << "ÇëÊäÈë°ë¾¶" << endl;
+	cin >> r;
+	s = Pi*r*r;
+	cout << "s=" << s << endl;
+	return 0;
+}
